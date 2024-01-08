b51d887d0998254e50543b531655183a7d327b48
Daniel Schömer <daniel.schoemer@gmx.net>
2024-01-08 21:00:47 +0100

    Add dialyxir

--- mix.exs
+++ mix.exs
@@ -55,6 +55,7 @@ defmodule Pento.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, ">= 0.0.0"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:styler, "~> 0.11", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false}
     ]
