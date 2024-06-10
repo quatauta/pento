4b8458dceaf87ca164097046ae537ca6885f3d5e
Daniel Schömer <daniel.schoemer@gmx.net>
2024-06-10 07:57:53 +0200

    Update dependencies - config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,7 @@ config :pento, Pento.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.21.4",
+  version: "0.21.5",
   default: [
     args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
