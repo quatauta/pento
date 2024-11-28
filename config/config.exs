563a7ec7da1128a8c10222187bc717bfac0137cc
Daniel Schömer <daniel.schoemer@gmx.net>
2024-11-28 15:22:10 +0100

    Update dependencies - config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -42,7 +42,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.4.14",
+  version: "3.4.15",
   default: [
     args: ~w(
       --config=tailwind.config.js
